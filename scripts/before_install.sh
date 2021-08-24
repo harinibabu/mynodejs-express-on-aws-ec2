918dd8a7b0ecd182902dc6a1fb42110d62008ed8
Harini Babu <HariniB@emids.com>
2021-08-24 21:21:33 +0530

    node install commands fixed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #download node and npm
-curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
 
 #create out working directory if it dosen't exist
 DIR="/home/ec2-user/express-app"
